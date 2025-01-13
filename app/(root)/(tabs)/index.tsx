b5c97f82e32fa25b2f6d65a67dd1f201e2ccadc0
MoniqChege <quichege33@gmail.com>
2025-01-13 16:07:43 +0300

    commit

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -9,6 +9,7 @@ export default function Index() {
       justifyContent: "center",
       alignItems: "center",
     }}>
+      <Text className="font-bold text-lg my-11">Welcome to ReState</Text>
     <Link href="/sign-in">Sign In</Link>
     <Link href="/explore">Explore</Link>
     <Link href="/profile">Profile</Link>
