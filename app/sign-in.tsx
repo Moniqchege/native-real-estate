4eb07659d01ac32af1f2b28503c772a06dbb3a17
MoniqChege <quichege33@gmail.com>
2025-01-14 11:51:29 +0300

    commit

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,9 +1,10 @@
-import { Image, ScrollView, StyleSheet, Text, View } from 'react-native'
+import { Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import images from '@/constants/images'
 
 const SignIn = () => {
+  const handleLogin = () => {};
   return (
     <SafeAreaView className='bg-white h-full'>
       <ScrollView contentContainerClassName='h-full'>
@@ -12,6 +13,16 @@ const SignIn = () => {
           <Text className='text-base text-center uppercase font-rubik text-black-200'>
             Welcome to Restate
           </Text>
+          <Text className='text-3xl font-rubik-bold text-black-300 text-center mt-2'>
+            Let's Get Yu Closer to {"\n"}
+            <Text className='text-primary-300'>Your Ideal Home</Text>
+          </Text>
+          <Text className='text-lg font-rubik text-black-200 text-center mt-12'>
+            Login to ReState with Google
+          </Text>
+          <TouchableOpacity onPress={handleLogin}>
+
+          </TouchableOpacity>
         </View>
       </ScrollView>
     </SafeAreaView>
